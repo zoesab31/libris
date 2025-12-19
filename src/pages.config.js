1d72ace0a9b103c6d6a15cf6ef8fac7485ee2511
Zoé Sabourin <zoesab31@gmail.com>
2025-12-19 23:02:33 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,13 +1,16 @@
+import AccountSettings from './pages/AccountSettings';
 import Authors from './pages/Authors';
 import Bingo from './pages/Bingo';
 import BookTournament from './pages/BookTournament';
 import Chat from './pages/Chat';
+import Dashboard from './pages/Dashboard';
 import Discover from './pages/Discover';
 import FanArt from './pages/FanArt';
 import Friends from './pages/Friends';
 import Home from './pages/Home';
 import Maps from './pages/Maps';
 import MusicPlaylist from './pages/MusicPlaylist';
+import MyLibrary from './pages/MyLibrary';
 import NailInspo from './pages/NailInspo';
 import OneSignalGuide from './pages/OneSignalGuide';
 import Profile from './pages/Profile';
@@ -16,26 +19,26 @@ import Series from './pages/Series';
 import SeriesTracking from './pages/SeriesTracking';
 import SharedReadings from './pages/SharedReadings';
 import ShelfView from './pages/ShelfView';
+import Statistics from './pages/Statistics';
 import UserProfile from './pages/UserProfile';
 import VirtualLibrary from './pages/VirtualLibrary';
-import MyLibrary from './pages/MyLibrary';
-import Statistics from './pages/Statistics';
-import Dashboard from './pages/Dashboard';
-import AccountSettings from './pages/AccountSettings';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
+    "AccountSettings": AccountSettings,
     "Authors": Authors,
     "Bingo": Bingo,
     "BookTournament": BookTournament,
     "Chat": Chat,
+    "Dashboard": Dashboard,
     "Discover": Discover,
     "FanArt": FanArt,
     "Friends": Friends,
     "Home": Home,
     "Maps": Maps,
     "MusicPlaylist": MusicPlaylist,
+    "MyLibrary": MyLibrary,
     "NailInspo": NailInspo,
     "OneSignalGuide": OneSignalGuide,
     "Profile": Profile,
@@ -44,12 +47,9 @@ export const PAGES = {
     "SeriesTracking": SeriesTracking,
     "SharedReadings": SharedReadings,
     "ShelfView": ShelfView,
+    "Statistics": Statistics,
     "UserProfile": UserProfile,
     "VirtualLibrary": VirtualLibrary,
-    "MyLibrary": MyLibrary,
-    "Statistics": Statistics,
-    "Dashboard": Dashboard,
-    "AccountSettings": AccountSettings,
 }
 
 export const pagesConfig = {
